69925e41d52cd1c9a688ab3c0b386bf46510d45c
bicasino <76057429+bicasino@users.noreply.github.com>
2022-11-19 00:25:19 +0700

    19/11

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,13 @@
 let dropdownBtn = document.querySelector('#menubar_respon');
 let menuContent = document.querySelector('aside');
+let tableContent = document.querySelector('#table-respon');
 dropdownBtn.addEventListener('click',()=>{
    if(menuContent.style.display===""){
       menuContent.style.display="block";
+      tableContent.style.display="";
    } else {
       menuContent.style.display="";
+      tableContent.style.display="block";
    }
 })
 // document.getElementById("menubar_respon").onclick = function() {  
